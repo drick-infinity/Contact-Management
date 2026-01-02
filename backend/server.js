a83d7b2ac72a3eb1aa72e0f0f9b74fbc22da594a
drick-infinity <harshimalviya47487@gmail.com>
2026-01-02 21:34:24 +0530

    server.js modified

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDb from "./config/db.js";
 import contactRoutes from "./routes/contacts.js";
 
@@ -14,6 +17,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
